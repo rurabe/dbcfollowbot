befb16805d07570e3a7488f20429d3225b8b0f07
rurabe <rurabe@gmail.com>
2012-06-17 21:23:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbcfollowbot::Application.config.secret_token = '215e7e698732495fcf6e3a6fbe322f43df2497de92d47141be6b5156a03cfc39e23f4d796dbc66378cd43713e7bd62af3880efaed77b57f414b3248da38101b0'
